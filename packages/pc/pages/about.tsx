1ce383e5c25cdca11ab362e789e436ceef05c2df
Hyeungshik Jung <hyungshike@gmail.com>
2020-02-15 19:32:40 +0900

    feat: Add about page

---
+++ packages/pc/pages/about.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const About = () => (<>
+<h1>Just another demo</h1>
+<Link href="/">back to home</Link>
+</>
+);
+
+export default About;
--- packages/pc/pages/index.tsx
+++ packages/pc/pages/index.tsx
@@ -1,3 +1,9 @@
-const Home = () => <h1>Hello world!</h1>;
+import Link from 'next/link';
+
+
+const Home = () => <>
+<h1>Hello world!</h1>
+<Link href="/about">about</Link>
+</>;
 
 export default Home;
