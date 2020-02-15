09ed325062ce6bdd2b2b7a2400f701a2faf3ca3b
Hyeungshik Jung <hyungshike@gmail.com>
2020-02-15 22:46:08 +0900

    chore: Add sass support

--- packages/pc/next.config.js
+++ packages/pc/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withSass = require('@zeit/next-sass')
+
+module.exports = withSass({
   webpack(config) {
     config.resolve.modules.push(__dirname)
     return config;
   },
-}
+})
--- packages/pc/src/components/clock.tsx
+++ packages/pc/src/components/clock.tsx
@@ -1,19 +1,9 @@
+import './clock.scss'
+
 export default ({ lastUpdate, light }) => {
   return (
-    <div className={light ? 'light' : ''}>
+    <div className={light ? 'clock light' : 'clock'}>
       {format(new Date(lastUpdate))}
-      <style jsx>{`
-        div {
-          padding: 15px;
-          display: inline-block;
-          color: #82fa58;
-          font: 50px menlo, monaco, monospace;
-          background-color: #000;
-        }
-        .light {
-          background-color: #999;
-        }
-      `}</style>
     </div>
   )
 }
